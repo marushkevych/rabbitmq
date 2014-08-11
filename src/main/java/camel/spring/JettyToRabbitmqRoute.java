8a9ea7cc612f65d792b671539d12b59803db6394
marushkevych <marushkevych@gmail.com>
2014-08-11 08:56:06 -0400

    rpc

--- src/main/java/camel/spring/JettyToRabbitmqRoute.java
+++ src/main/java/camel/spring/JettyToRabbitmqRoute.java
@@ -3,6 +3,12 @@ package camel.spring;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Use ROUTING_KEY parameter when requesting jetty webservice:
+ * 
+ * http://localhost:9898?ROUTING_KEY
+ *
+ */
 @Component
 public class JettyToRabbitmqRoute extends RouteBuilder{
     
